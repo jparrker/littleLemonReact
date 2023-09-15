a56845e6f4bfe00fd8bae5d2465240ca6fbe8dd6
jparrker <102701917+jparrker@users.noreply.github.com>
2023-09-15 17:05:05 -0400

    added boiler plate for components and started building out navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import pages from "./utils/pages";
+import Layout from "./layouts/Layout/Layout";
+import Menu from "./pages/Menu/Menu";
+import Orders from "./pages/Orders/Orders";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+
+import Reservations from "./pages/Reservations/Reservations";
 
 function App() {
-  return <div className="App">Hello react</div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path={pages.get("home".path)} element={<Home />} />
+        <Route path={pages.get("about".path)} element={<About />} />
+        <Route path={pages.get("menu".path)} element={<Menu />} />
+        <Route
+          path={pages.get("reservations".path)}
+          element={<Reservations />}
+        />
+        <Route path={pages.get("orders".path)} element={<Orders />} />
+        <Route path={pages.get("login".path)} element={<Login />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/layouts/Navbar/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import Logo from "../../assets/Logo.svg";
+import MenuIcon from "../../assets/hamburger.svg";
+import { useNavigate } from "react-router-dom";
+
+function Navbar() {
+  const nav = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  const handleHomeClick = () => {
+    nav("/");
+  };
+  const menu = [
+    { label: "Home", link: "/" },
+    { label: "About", link: "/about" },
+    { label: "Menu", link: "/menu" },
+    { label: "Reservations", link: "/reservations" },
+    { label: "Order Online", link: "/orders" },
+    { label: "Login", link: "/login" },
+  ];
+  return (
+    <header className="navbar">
+      <div className="logo">
+        <img
+          src={Logo}
+          height={50}
+          alt="litle lemon logo"
+          onClick={handleHomeClick}
+        />
+      </div>
+      <nav className={`nav- ${menuOpen ? "open" : ""}`}>
+        <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+          {menu.map((item) => (
+            <li key={item.label}>
+              <a href={item.link} className="link">
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <img src={MenuIcon} alt="nav toggle" />
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;

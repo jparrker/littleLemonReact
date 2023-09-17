83503edd644a7878ecef45838beb1182dd80ff84
jparrker <102701917+jparrker@users.noreply.github.com>
2023-09-17 16:44:45 -0400

    added card logic, hero, and css

--- src/pages/Reservations/Reservations.js
+++ src/pages/Reservations/Reservations.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Footer from "../../layouts/Footer/Footer";
+import Navbar from "../../layouts/Navbar/Navbar";
+import BookingForm from "../../components/BookingForm/BookingForm";
 function Reservations() {
   return <div>Reservations</div>;
 }
